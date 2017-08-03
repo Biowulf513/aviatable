7b66a34c23854f3680bd206bbe29d2fb67030145
Biowulf513 <cherepanov92@gmail.com>
2017-08-03 16:31:47 +0500

    переработка логики работы search

--- flight/urls.py
+++ flight/urls.py
@@ -20,11 +20,10 @@ Including another URLconf
 
 from django.conf.urls import url
 from flight.views import one_route
-from flight.search import all_search, form_search
+from flight.search import all_search
 
 urlpatterns = [
     url(r'^(?P<route_id>[0-9]+)/$', one_route, name='one_route'),
     url(r'^search/$', all_search),
-    url(r'^search/form/$', form_search),
 ]
 
