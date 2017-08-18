0113cab5d3cf463866412dc22f3f0614a0d4ee09
Biowulf513 <cherepanov92@gmail.com>
2017-08-18 16:36:35 +0500

    обязательные поля при регистрации

--- flight/search.py
+++ flight/search.py
@@ -8,18 +8,24 @@ from flight.utils import search_lang
 def all_search(request, query=''):
     if request.GET:
         context = {}
-        if request.GET.get('search_q'):
-            query=request.GET['search_q']
-            if search_lang(query) != 'ru':
-                context['routes'] = Route.objects.filter(
-                  Q(code__icontains=query) | Q(plane__reg_numb__icontains=query)
-                )
-            else:
-                query = query.capitalize()
-                context['routes'] = Route.objects.filter(
-                    Q(airport_out__name__icontains=query) | Q(airport_in__name__icontains=query)
-                )
+        query=request.GET['search_q']
+        if search_lang(query) != 'ru':
+            context['routes'] = Route.objects.filter(
+              Q(code__icontains=query) | Q(plane__reg_numb__icontains=query)
+            )
+        else:
+            query = query.capitalize()
+            context['routes'] = Route.objects.filter(
+                Q(airport_out__name__icontains=query) | Q(airport_in__name__icontains=query)
+            )
+
+        if context['routes']:
             return render(request, 'flight/search_route.html', context)
+
+        else:
+            context.clear()
+            context['message'] = 'Рейса с данными параметрами не найдено!'
+        return render(request, 'flight/search_route.html', context)
 
 def filter_search(request, ):
     if request.GET:
